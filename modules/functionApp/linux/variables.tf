728d3d8188cffe9c6bdedafe6dd87988d9d97519
Kenrick Lawrence <51682573+S7evenCalevra@users.noreply.github.com>
2022-04-24 15:29:57 -0400

    Create variables.tf

---
+++ modules/functionApp/linux/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  type = string
+  description = "Project name"
+}
+
+variable "environment" {
+  type = string
+  #description = "Environment (dev / stage / prod)"
+  description = "dev"
+}
+
+variable "location" {
+  type = string
+  description = "Azure region"
+}
