9c3123ba62d60ee0da83151fbccbff8e3502cc6b
Kenrick Lawrence <51682573+S7evenCalevra@users.noreply.github.com>
2022-07-10 18:41:20 -0400

    Update main.tf

--- modules/WindowsVM/main.tf
+++ modules/WindowsVM/main.tf
@@ -1,16 +1,16 @@
-data "azurerm_resource_group" "keyvault_rg" {
-  name = "${var.keyvault_rg}"
-}
+#data "azurerm_resource_group" "keyvault_rg" {
+#  name = "${var.keyvault_rg}"
+#}
 
-data "azurerm_key_vault" "keyvault" {
-  name = "${var.keyvault_name}"
-  resource_group_name = "${data.azurerm_resource_group.keyvault_rg.name}"
-}
+#data "azurerm_key_vault" "keyvault" {
+#  name = "${var.keyvault_name}"
+#  resource_group_name = "${data.azurerm_resource_group.keyvault_rg.name}"
+#}
 
-data "azurerm_key_vault_secret" "VmToken" {
-  name = "${var.keyvault_token}"
-  key_vault_id = "${data.azurerm_key_vault.keyvault.id}"
-}
+#data "azurerm_key_vault_secret" "VmToken" {
+#  name = "${var.keyvault_token}"
+#  key_vault_id = "${data.azurerm_key_vault.keyvault.id}"
+#}
 
 
 
@@ -101,18 +101,18 @@ resource "azurerm_virtual_machine_data_disk_attachment" "vm_datadisk2_attach" {
   caching            = "ReadWrite"
 }
 
-resource "azurerm_managed_disk" "vm_datadisk3" {
-  name                 = "${var.vm_machine_name}-disk3"
-  resource_group_name = data.azurerm_resource_group.vm_rg.name
-  location            = data.azurerm_resource_group.vm_rg.location
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = var.vm_data_disk_size_3
-}
+#resource "azurerm_managed_disk" "vm_datadisk3" {
+#  name                 = "${var.vm_machine_name}-disk3"
+#  resource_group_name = data.azurerm_resource_group.vm_rg.name
+#  location            = data.azurerm_resource_group.vm_rg.location
+#  storage_account_type = "Standard_LRS"
+#  create_option        = "Empty"
+#  disk_size_gb         = var.vm_data_disk_size_3
+#}
 
-resource "azurerm_virtual_machine_data_disk_attachment" "vm_datadisk3_attach" {
-  managed_disk_id    = azurerm_managed_disk.vm_datadisk3.id
-  virtual_machine_id = azurerm_windows_virtual_machine.vm_winvm.id
-  lun                = "12"
-  caching            = "ReadWrite"
-}
+#resource "azurerm_virtual_machine_data_disk_attachment" "vm_datadisk3_attach" {
+#  managed_disk_id    = azurerm_managed_disk.vm_datadisk3.id
+#  virtual_machine_id = azurerm_windows_virtual_machine.vm_winvm.id
+#  lun                = "12"
+#  caching            = "ReadWrite"
+#}
