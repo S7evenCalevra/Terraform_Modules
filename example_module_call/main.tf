df73c593ce143f9306f0bd76729c545492bca058
Kenrick Lawrence <51682573+S7evenCalevra@users.noreply.github.com>
2022-07-11 17:41:10 -0400

    Update main.tf

--- example_module_call/main.tf
+++ example_module_call/main.tf
@@ -13,12 +13,8 @@ module "windows_vm" {
     vm_network_subnet = "internal"
     vm_machine_name = "WinTestVM"
     vm_size = "Standard_B2s"
-    vm_network_interface_count = "2"
+    vm_network_interface_count = "1"
     vm_os_disk_size = 127
     vm_data_disk_size_1 = 20
-    vm_data_disk_size_2 = 15
-    vm_data_disk_size_3 = 10
     keyvault_rg = "TerraformIACTest"
-    keyvault_name = "terraffirniac-kv"
-    keyvault_token = "testVMToken"
 }
